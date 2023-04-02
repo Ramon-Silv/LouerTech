c300d0332663e480daf9d29f33d5c8bb150786ab
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-02 20:47:58 -0300

    ok

---
+++ orcamento/orcamento.js
@@ -0,0 +1,7 @@
+let span_frete = document.getElementById("span_frete");
+let span_valor_total = document.getElementById("span_valor_total");
+
+function mostrar_valores() {
+  span_frete.innerHTML = "5,00";
+  span_valor_total.innerHTML = "105,00";
+}
