2c4a05ada1402c8599756119293f1042091a4b35
Miguel Barbosa Cavalcante Mendes <miguelbarbosa763@gmail.com>
2023-04-02 21:01:34 -0300

    ok

--- orcamento/orcamento.js
+++ orcamento/orcamento.js
@@ -3,5 +3,5 @@ let span_valor_total = document.getElementById("span_valor_total");
 
 function mostrar_valores() {
   span_frete.innerHTML = "100,00";
-  span_valor_total.innerHTML = "105,00";
+  span_valor_total.innerHTML = "387,00";
 }
